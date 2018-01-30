ed5376a3e0c18ed3ecfd48cbb173a4aa707b6fe7
casterlhanog <guinnesscoder@gmail.com>
2018-01-30 11:28:51 -0200

    1.0

--- Version 2(1.0).py
+++ Version 2(1.0).py
@@ -2,8 +2,10 @@ import csv
 
 class Depuradora(object):
     def __init__(self, a, b):
-        self.arquivo1 = csv.reader(open('%s.csv'%(a)), delimiter=';')
-        self.arquivo2 = csv.reader(open('%s.csv'%(b)), delimiter=';')
+        self.a = a
+        self.b = b
+        self.arquivo1 = csv.reader(open('%s.csv'%(self.a)), delimiter=';')
+        self.arquivo2 = csv.reader(open('%s.csv'%(self.b)), delimiter=';')
 
         self.inicia()
 
@@ -17,7 +19,22 @@ class Depuradora(object):
         for j in self.arquivo2:
             lista2.append(j)
 
-arq1 = input("Digite o nome do primeiro arquivo (sem a extensão CSV)")
-arq2 = input("Digite o nome do segundo arquivo(sem a extensão CSV)")
+        self.verifica(lista1, lista2)
+
+    def verifica(self, l1,l2):
+
+        if len(l1) > len(l2):
+            print('Existem lançamentos a maior em %s.' % (self.a))
+        else:
+            print('Existem lançamentos a maior em %s.' % (self.b))
+
+        analisa()
+
+    def analisa(self):
+        
+
+
+arq1 = input("Digite o nome do primeiro arquivo (sem a extensão CSV): ")
+arq2 = input("Digite o nome do segundo arquivo(sem a extensão CSV): ")
 
 Depuradora(arq1, arq2)
